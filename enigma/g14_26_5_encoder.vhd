c2e4671c4ef3016c4371d74598bf33feca5a64c2
Malcolm Watt <malcolm.watt@mail.mcgill.ca>
2016-02-04 14:35:26 -0500

    Modify template file for decoder.

--- enigma/g14_26_5_encoder.vhd
+++ enigma/g14_26_5_encoder.vhd
@@ -2,16 +2,22 @@
 -- INDEX which indicates the highest bit position over all input bits that have
 -- a high value. If no inputs bits are high, then assert an ERROR signal.
 --
--- entity name: g00_26_5_encoder
+-- entity name: g14_26_5_encoder
 --
--- Copyright (C) 2016 Warren Gross
--- Version 1.0
--- Author: Warren Gross; warren.gross@mcgill.ca
--- Date: January 1, 2016
+-- Copyright (C) 2016 Malcolm Watt, Joanna Halpern
+-- Version 0.0.1
+--
+-- Authors: Malcolm Watt (260585950); malcolm.watt@mail.mcgill.ca
+-- 			Joanna Halpern (260410826); joanna.halpern@mail.mcgill.ca
+--
+-- Date: February 4 2016
+
 library ieee; -- allows use of the std_logic_vector type
 use ieee.std_logic_1164.all;
-entity g00_26_5_encoder is
-port ( letter : in std_logic_vector(25 downto 0);
- INDEX : out std_logic_vector(4 downto 0);
- ERROR : out std_logic);
-end g00_26_5_encoder; 
+use ieee.numeric_std.all;
+
+entity g14_26_5_encoder is
+port ( LETTER : in std_logic_vector(25 downto 0);
+       INDEX : out std_logic_vector(4 downto 0);
+       ERROR : out std_logic);
+end g14_26_5_encoder; 
